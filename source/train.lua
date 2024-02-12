aea0dceb8273d030b473a0bdd2272deb4f4a6a44
Nate Beaty <nate@clixel.com>
2024-02-11 22:06:31 -0500

    add train collisions

--- source/train.lua
+++ source/train.lua
@@ -1,6 +1,5 @@
 import "CoreLibs/sprites"
 import "lib/AnimatedSprite.lua"
-import "lib/state"
 
 local gfx <const> = playdate.graphics
 local point <const> = playdate.geometry.point
@@ -13,7 +12,7 @@ animTimer.repeats = true
 
 local minXPosition = -100
 local maxXPosition = 500
-local yPosition = 223
+local yPosition = 231
 
 local trainImagesTable = gfx.imagetable.new("images/train")
 
@@ -27,7 +26,7 @@ function Train:init()
   self:setImage(self.imagesTable:getImage(1))
   self:setZIndex(1000)
   local sw,sh = trainImagesTable:getImage(1):getSize()
-  self:setCollideRect(sw/4, sh/4, sw/2, sh/2)
+  self:setCollideRect(4, 4, sw-8, sh-8)
   self:setGroups({3})
   self:setCollidesWithGroups({1})
   self.collisionResponse = gfx.sprite.kCollisionTypeOverlap
@@ -42,8 +41,14 @@ end
 function Train:update()
   if self.launched then
     self.position += self.velocity
-    -- print("supply pos", self.position, self.velocity)
-    self:moveTo(self.position)
+
+    local x,y,c,l = self:moveWithCollisions(self.position)
+    for i = 1, l do
+      local other = c[i].other
+      if other.isa(Player) then
+        other:die()
+      end
+    end
 
     if self.position.x < minXPosition or self.position.x > maxXPosition then
       self.launched = false
@@ -62,9 +67,10 @@ end
 function Train:launch()
   self.position.x = rnd()>0.35 and -40 or screenWidth + 40
   self.velocity = vector2D.new(self.position.x < 0 and 3 or -3, 0)
+  self:setImage(self.imagesTable:getImage(1))
   -- express?
-  if rnd()>0.99 then
-    self.velocity *= 1.5
+  if rnd()>0.7 then
+    self.velocity *= 2
     self:setImage(self.imagesTable:getImage(2))
   end
   self:moveTo(self.position)
