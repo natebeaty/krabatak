f4e76a72e8a69e1390efeee8f1103169feec4574
Nate Beaty <nate@clixel.com>
2024-02-11 22:04:11 -0500

    add collision boxes for floor borders and playerModeSwitch areas

--- source/levels/city.lua
+++ source/levels/city.lua
@@ -3,9 +3,39 @@ import "CoreLibs/sprites"
 class("City").extends()
 
 local gfx <const> = playdate.graphics
-local background = gfx.image.new('images/background-tall')
+local background = gfx.image.new("images/background-tall")
+
+local function addCollisionBox(x,y,w,h,flag)
+  flag = flag or "wall"
+  local box = gfx.sprite:new()
+  -- box:setIgnoresDrawOffset(true)
+  box:setZIndex(1000)
+  box:moveTo(x,y)
+  box:setCenter(0,0) -- set center point to left bottom
+  box:setSize(w,h)
+  box:setCollideRect(0,0,w,h)
+  box:setGroups({1})
+  box:setCollidesWithGroups({1})
+  -- box.collisionResponse = gfx.sprite.kCollisionTypeSlide
+  box:addSprite()
+  box.flag = flag
+end
 
 function City:init()
+  -- screen borders
+  -- addCollisionBox(-5,0,5,240)
+  -- addCollisionBox(400,0,5,240)
+  -- addCollisionBox(0,0,400,25)
+
+  -- floor borders
+  addCollisionBox(24,203,7,19)
+  addCollisionBox(30,216,339,6)
+  addCollisionBox(369,203,7,19)
+
+  -- man/plane changes
+  addCollisionBox(0,201,31,5,"playerModeSwitch")
+  addCollisionBox(370,201,31,5,"playerModeSwitch")
+
   self.by = -240
   self.sprite = gfx.sprite.setBackgroundDrawingCallback(
     function(x, y, width, height)
