946e431a9b174239466d74ec9599bd2c3c776a56
Nate Beaty <nate@clixel.com>
2024-02-11 22:06:25 -0500

    fix supply collisions

--- source/supply.lua
+++ source/supply.lua
@@ -1,6 +1,6 @@
+
 import "CoreLibs/sprites"
 import "lib/AnimatedSprite.lua"
-import "lib/state"
 
 local gfx <const> = playdate.graphics
 local point <const> = playdate.geometry.point
@@ -8,13 +8,13 @@ local vector2D <const> = playdate.geometry.vector2D
 
 class("Supply").extends(gfx.sprite)
 
-local animTimer = playdate.frameTimer.new(6)
+local animTimer = playdate.frameTimer.new(2)
 animTimer.repeats = true
 
 local minXPosition = -100
 local maxXPosition = 500
 local minYPosition = -230
-local maxYPosition = 260
+local maxYPosition = 208
 
 local supplyImagesTable = gfx.imagetable.new("images/supply")
 local balloonImagesTable = gfx.imagetable.new("images/balloon")
@@ -26,7 +26,7 @@ local balloon = AnimatedSprite.new(balloonImagesTable)
 balloon:setCollideRect(0, 0, balloonImagesTable:getImage(1):getSize())
 balloon:setGroups({3})
 balloon:setCollidesWithGroups({1})
-balloon.collisionResponse = gfx.sprite.kCollisionTypeOverlap
+-- balloon.collisionResponse = gfx.sprite.kCollisionTypeOverlap
 balloon:addState("open", 3, 4, { tickStep = 2 })
 balloon:addState("parachute", 1, 2, { tickStep = 2, loop = 5, nextAnimation = "open" }).asDefault()
 balloon.states["parachute"].onAnimationEndEvent = function()
@@ -38,25 +38,22 @@ balloon.launched = false
 function balloon:update()
   if self.launched then
     self.position += self.velocity
-    if (self.position.y < minYPosition) then
-      self.launched = false
-      self:stopAnimation()
-      self:remove()
+    if (self.position.y > maxYPosition) then
+      self:die()
     end
     local x,y,c,l = self:moveWithCollisions(self.position)
     for i=1,l do
       local other = c[i].other
-      if other:isa(Enemy) then
+      if other.isEnemy then
         other:die()
         self:die()
-      end
-      if other:isa(Player) then
+      elseif other:isa(Player) then
+
         player:refuel()
         self.launched = false
         self:stopAnimation()
         self:remove()
-      end
-      if other:isa(Block) then
+      elseif other:isa(Block) then
         other:hit()
         self:die()
       end
@@ -64,18 +61,31 @@ function balloon:update()
     self:updateAnimation()
   end
 end
+
+-- function balloon:collisionResponse(other)
+--   if other:isa(Player) then
+--     return "overlap"
+--   else
+--     return "freeze"
+--   end
+-- end
+
 function balloon:die()
   self.launched = false
   animations:explosion(self.position.x, self.position.y)
   self:stopAnimation()
   self:remove()
 end
+
 function balloon:launch()
   self:addSprite()
   self.launched = true
   self:moveTo(self.position)
   self:playAnimation()
 end
+
+
+-- Supply ship
 
 function Supply:init()
   Supply.super.init(self)
@@ -86,7 +96,7 @@ function Supply:init()
   self:setCollideRect(5, 5, 46, 10)
   self:setGroups({3})
   self:setCollidesWithGroups({1})
-  self.collisionResponse = gfx.sprite.kCollisionTypeOverlap
+  -- self.collisionResponse = gfx.sprite.kCollisionTypeOverlap
 
   self.position = point.new(-200,0)
   self:moveTo(self.position)
@@ -110,22 +120,17 @@ function Supply:update()
 
     for i=1,l do
       local other = c[i].other
-      if other:isa(Enemy) then
+      if other.isEnemy then
         other:die()
         self:die()
-      end
-      if other:isa(Player) then
+      elseif other:isa(Player) then
         other:die()
-        self:die()
-      end
-      if other:isa(Block) then
-        other:hit()
         self:die()
       end
     end
 
     -- animate jet
-    self:setImage(self.imagesTable:getImage(animTimer.frame < 2 and 1 or 2), self.velocity.x < 0 and '' or 'flipX')
+    self:setImage(self.imagesTable:getImage(animTimer.frame < 1 and 1 or 2), self.velocity.x < 0 and '' or 'flipX')
 
     if self.position.x < minXPosition or self.position.x > maxXPosition then
       self.launched = false
@@ -142,7 +147,9 @@ function Supply:update()
     end
 
   else
+
     self:checkLaunch()
+
   end
 
   -- offscreen?
@@ -150,7 +157,7 @@ function Supply:update()
 end
 
 function Supply:checkLaunch()
-  if (not balloon.launched and not self.launched and rnd()>0.99) then
+  if (not balloon.launched and not self.launched and rnd()>0.995) then
     self:launch()
   end
 end
