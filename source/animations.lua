cba94159a5c676fecf28d3f15779a55ec2fc016a
Nate Beaty <nate@clixel.com>
2024-02-27 20:00:39 -0500

    add bonusYay animation

--- source/animations.lua
+++ source/animations.lua
@@ -4,11 +4,12 @@ class("Animations").extends()
 
 local gfx = playdate.graphics
 local explosionImages = gfx.imagetable.new("images/explosion")
+local bonusYayImages = gfx.imagetable.new("images/bonus-yay")
 
 function Animations:explosion(x,y)
-  local explosionAnimation = AnimatedSprite.new(explosionImages)
-  explosionAnimation:setZIndex(1100)
-  explosionAnimation:setStates({
+  local anim = AnimatedSprite.new(explosionImages)
+  anim:setZIndex(1100)
+  anim:setStates({
     {
       name = "explode",
       loop = 1,
@@ -18,6 +19,23 @@ function Animations:explosion(x,y)
       end
     }
   })
-  explosionAnimation:moveTo(x, y)
-  explosionAnimation:playAnimation()
+  anim:moveTo(x, y)
+  anim:playAnimation()
+end
+
+function Animations:bonusYay(x,y)
+  local anim = AnimatedSprite.new(bonusYayImages)
+  anim:setZIndex(1100)
+  anim:setStates({
+    {
+      name = "yay",
+      loop = 1,
+      tickStep = 2,
+      onAnimationEndEvent = function(self)
+        self:remove()
+      end
+    }
+  })
+  anim:moveTo(x, y)
+  anim:playAnimation()
 end
